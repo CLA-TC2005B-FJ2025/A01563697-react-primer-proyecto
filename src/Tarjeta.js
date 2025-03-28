d0765e6b5056803d46848304257114ea286be464
alf77mevi <a01563697@tec.mx>
2025-03-28 16:09:53 +0000

    cambios

--- src/Tarjeta.js
+++ src/Tarjeta.js
@@ -1,14 +1,21 @@
 import React from 'react'
 
-export function Tarjeta( props ) {
+export function Tarjeta( props ) {  // se le puede poner otro nombre
   return (
-    <div>
-      <div>
-        Nombre: {props.nombre}
+    <div className="card">
+        <div className="card-image">
+          <figure className="image is-1by1">
+            <img src={props.image} alt="pda logo" />
+          </figure>
+        </div>
+  
+        <div className="card-content">
+          <div className="media-content">
+            <p className="title is-4">{props.nombre}</p>
+            <p className="subtitle is-6">{props.colorFavorito} es mi color favorito</p>
+          </div>
+        </div>
       </div>
-      <div>
-        Color favorito: {props.colorFavorito}
-      </div>
-    </div>
+  
   )
 }
